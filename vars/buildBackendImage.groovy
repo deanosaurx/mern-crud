0ad3971b1f512aaa28d547bd94f703142e323e6b
Dean Sorie <deansorie@icloud.com>
2023-04-02 10:09:16 +0300

    Added sudo to groovy file

--- vars/buildBackendImage.groovy
+++ vars/buildBackendImage.groovy
@@ -1,7 +1,7 @@
 def call() {
     script {
-        sh 'docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD'
-        sh 'docker build -t ${DOCKER_REGISTRY}/${BACKEND_IMAGE_NAME}:${BACKEND_IMAGE_TAG} ./backend'
-        sh 'docker push ${DOCKER_REGISTRY}/${BACKEND_IMAGE_NAME}:${BACKEND_IMAGE_TAG}'
+        sh 'sudo docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD'
+        sh 'sudo docker build -t ${DOCKER_REGISTRY}/${BACKEND_IMAGE_NAME}:${BACKEND_IMAGE_TAG} ./backend'
+        sh 'sudo docker push ${DOCKER_REGISTRY}/${BACKEND_IMAGE_NAME}:${BACKEND_IMAGE_TAG}'
     }
 }
