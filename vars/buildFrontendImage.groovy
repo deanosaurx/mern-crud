d18e57e00308e616753106af456bdaabb5dbbe01
Dean Sorie <deansorie@icloud.com>
2023-04-02 10:09:46 +0300

    Adde sudo to both groovy files

--- vars/buildFrontendImage.groovy
+++ vars/buildFrontendImage.groovy
@@ -1,7 +1,7 @@
 def call() {
     script {
-        sh 'docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD'
-        sh 'docker build -t ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:${FRONTEND_IMAGE_TAG} ./frontend'
-        sh 'docker push ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:${FRONTEND_IMAGE_TAG}'
+        sh 'sudo docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD'
+        sh 'sudo ocker build -t ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:${FRONTEND_IMAGE_TAG} ./frontend'
+        sh 'sudo docker push ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:${FRONTEND_IMAGE_TAG}'
     }
 }
