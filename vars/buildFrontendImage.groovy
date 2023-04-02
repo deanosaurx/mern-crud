489737346f620191abbfae352950e1977a77e6bd
Dean Sorie <deansorie@icloud.com>
2023-04-02 14:29:41 +0300

    a freaking typo in jenkins shared libs

--- vars/buildFrontendImage.groovy
+++ vars/buildFrontendImage.groovy
@@ -7,7 +7,7 @@ def call() {
         sh "docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD"
         withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB', passwordVariable: 'DOCKER_HUB_PASSWORD', usernameVariable: 'DOCKER_HUB_USERNAME')]) {
         sh "docker push ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:latest"
-        sh "docker push ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:${IMAGE_TAG}""
+        sh "docker push ${DOCKER_REGISTRY}/${FRONTEND_IMAGE_NAME}:${IMAGE_TAG}"
         }
     }
 }
